2565ea3b19af020492e8a6a03e4fee101ba71e2a
YUDYlly <cocoselfer@gmail.com>
2026-01-15 19:47:33 +0900

    Switch Typekit font loading to JS embed method
    Replaces the synchronous Typekit CSS and preconnect links with the recommended JavaScript embed method for Adobe Fonts. This improves font loading performance and reliability.

--- app/layout.tsx
+++ app/layout.tsx
@@ -91,11 +91,21 @@ export default function RootLayout({
   return (
     <html lang="ja" className={`${notoSansJP.variable} ${notoSerifJP.variable}`}>
       <head>
-        {/* Typekit preconnect for faster font loading */}
-        <link rel="preconnect" href="https://use.typekit.net" crossOrigin="anonymous" />
-        <link rel="preconnect" href="https://p.typekit.net" crossOrigin="anonymous" />
-        {/* Typekit CSS - synchronous load for initial render */}
-        <link rel="stylesheet" href="https://use.typekit.net/hdf7hnv.css" />
+        {/* Adobe Fonts (Typekit) - JavaScript embed method */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(d) {
+                var config = {
+                  kitId: 'hdf7hnv',
+                  scriptTimeout: 3000,
+                  async: true
+                },
+                h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+              })(document);
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
